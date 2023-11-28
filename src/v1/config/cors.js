c0e44fe400813b2b9e5fbd2c86d57a490c3665a2
elif <elifdeniz@elif-MBP.home>
2023-11-28 16:36:19 +0300

    cors err

--- src/v1/config/cors.js
+++ src/v1/config/cors.js
@@ -1,12 +1,13 @@
 const whitelist = ["http://localhost:5173", "https://www.mongflow.com/"];
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (whitelist.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
+  origin: '*',
   methods: 'GET,PATCH,POST,DELETE,PUT',
   preflightContinue: false,
   optionsSuccessStatus: 204
